e6c74d76eda46ea901722958591348644f552d59
joq62 <joakim.leche@gmail.com>
2021-09-30 21:52:13 +0200

    created

---
+++ src/controller.app
@@ -0,0 +1,14 @@
+%% This is the application resource file (.app file) for the 'base'
+%% application.
+{application, controller,
+[{description, "Controller application and cluster" },
+{vsn, "0.1.0" },
+{modules, 
+	  [controller,controller_sup,controller_server]},
+{registered,[controller]},
+{applications, [kernel,stdlib]},
+{mod, {controller,[]}},
+{start_phases, []},
+{git_path,"https://github.com/joq62/controller.git"},
+{env,[{nodes,['controller@c0','controller@c2'",'controller@joq62-X550CA']}]}
+]}.
---
+++ test_src/test.erl
@@ -0,0 +1,195 @@
+%%% -------------------------------------------------------------------
+%%% Author  : uabjle
+%%% Description :  
+%%% 
+%%% Created : 10 dec 2012
+%%% -------------------------------------------------------------------
+-module(test).    
+     
+%% --------------------------------------------------------------------
+%% Include files
+%% --------------------------------------------------------------------
+%-include_lib("eunit/include/eunit.hrl").
+%% --------------------------------------------------------------------
+
+
+
+%% External exports
+-export([start/0]). 
+
+
+%% ====================================================================
+%% External functions
+%% ====================================================================
+
+
+%% --------------------------------------------------------------------
+%% Function:tes cases
+%% Description: List of test cases 
+%% Returns: non
+%% --------------------------------------------------------------------
+start()->
+    io:format("~p~n",[{"Start setup",?MODULE,?FUNCTION_NAME,?LINE}]),
+    ok=setup(),
+    io:format("~p~n",[{"Stop setup",?MODULE,?FUNCTION_NAME,?LINE}]),
+
+    
+    io:format("~p~n",[{"Start pass_0()",?MODULE,?FUNCTION_NAME,?LINE}]),
+    ok=pass_0(),
+    io:format("~p~n",[{"Stop pass_0()",?MODULE,?FUNCTION_NAME,?LINE}]),
+
+    io:format("~p~n",[{"Start init_start()",?MODULE,?FUNCTION_NAME,?LINE}]),
+    ok=init_start(),
+    io:format("~p~n",[{"Stop init_start()",?MODULE,?FUNCTION_NAME,?LINE}]),
+      %% End application tests
+    io:format("~p~n",[{"Start cleanup",?MODULE,?FUNCTION_NAME,?LINE}]),
+    ok=cleanup(),
+    io:format("~p~n",[{"Stop cleaup",?MODULE,?FUNCTION_NAME,?LINE}]),
+   
+    io:format("------>"++atom_to_list(?MODULE)++" ENDED SUCCESSFUL ---------"),
+    ok.
+
+
+%% --------------------------------------------------------------------
+%% Function:start/0 
+%% Description: Initiate the eunit tests, set upp needed processes etc
+%% Returns: non
+%% --------------------------------------------------------------------
+init_start()->
+
+    %% Load dbase_dist local
+    HostId=net_adm:localhost(),
+    DbaseNode=list_to_atom("dbase_dist@"++HostId),
+    rpc:call(DbaseNode,init,stop,[],2000),
+    os:cmd("rm -rf dbase_dist"),
+    io:format("git clone ~p~n",[os:cmd("git clone https://github.com/joq62/dbase_dist.git")]),
+  
+    true=code:add_patha("dbase_dist/ebin"),
+    ok=application:start(dbase_dist),
+    db_host_spec:create_table(),
+    db_host_spec:git_init(),
+    
+
+    %% Start Hosts HostId@HostId
+    
+    
+
+    %%
+    Env=appfile:read("controller.app",env),
+    {hosts,Hosts}=lists:keyfind(hosts,1,Env),
+    GitPath=appfile:read("controller.app",git_path),
+    io:format("Hosts, GitPath ~p~n",[{Hosts, GitPath}]),
+    ok.
+
+%% --------------------------------------------------------------------
+%% Function:start/0 
+%% Description: Initiate the eunit tests, set upp needed processes etc
+%% Returns: non
+%% --------------------------------------------------------------------
+pass_0()->
+    [{application,controller,
+      [{description,"Controller application and cluster"},
+       {vsn,"0.1.0"},
+       {modules,[controller,controller_sup,controller_server]},
+       {registered,[controller]},
+       {applications,[kernel,stdlib]},
+       {mod,{controller,[]}},
+       {start_phases,[]},
+       {git_path,"https://github.com/joq62/controller.git"},
+       {env,[{hosts,["c0","c2","joq62-X550CA"]}]}]}]=appfile:read("controller.app",all),
+    "https://github.com/joq62/controller.git"=appfile:read("controller.app",git_path),
+    [{hosts,["c0","c2","joq62-X550CA"]}]=appfile:read("controller.app",env),
+    {error,[eexists,glurk,read_app_file,appfile,_Line]}=appfile:read("controller.app",glurk),
+    {error,enoent}=appfile:read("glurk.app",git_path),
+    ok.
+
+%% --------------------------------------------------------------------
+%% Function:start/0 
+%% Description: Initiate the eunit tests, set upp needed processes etc
+%% Returns: non
+%% --------------------------------------------------------------------
+
+
+
+
+%% --------------------------------------------------------------------
+%% Function:start/0 
+%% Description: Initiate the eunit tests, set upp needed processes etc
+%% Returns: non
+%% --------------------------------------------------------------------
+pass_1()->
+   
+    
+    ok.
+
+%% --------------------------------------------------------------------
+%% Function:start/0 
+%% Description: Initiate the eunit tests, set upp needed processes etc
+%% Returns: non
+%% --------------------------------------------------------------------
+pass_2()->
+
+    ok.
+
+%% --------------------------------------------------------------------
+%% Function:start/0 
+%% Description: Initiate the eunit tests, set upp needed processes etc
+%% Returns: non
+%% --------------------------------------------------------------------
+pass_3()->
+
+    ok.
+
+%% --------------------------------------------------------------------
+%% Function:start/0 
+%% Description: Initiate the eunit tests, set upp needed processes etc
+%% Returns: non
+%% --------------------------------------------------------------------
+pass_4()->
+  
+    ok.
+
+%% --------------------------------------------------------------------
+%% Function:start/0 
+%% Description: Initiate the eunit tests, set upp needed processes etc
+%% Returns: non
+%% --------------------------------------------------------------------
+pass_5()->
+  
+    ok.
+
+
+
+
+%% --------------------------------------------------------------------
+%% Function:start/0 
+%% Description: Initiate the eunit tests, set upp needed processes etc
+%% Returns: non
+%% --------------------------------------------------------------------
+
+%% --------------------------------------------------------------------
+%% Function:start/0 
+%% Description: Initiate the eunit tests, set upp needed processes etc
+%% Returns: non
+%% --------------------------------------------------------------------
+
+setup()->
+
+  
+    ok.
+
+
+%% --------------------------------------------------------------------
+%% Function:start/0 
+%% Description: Initiate the eunit tests, set upp needed processes etc
+%% Returns: non
+%% -------------------------------------------------------------------    
+
+cleanup()->
+  
+    ok.
+%% --------------------------------------------------------------------
+%% Function:start/0 
+%% Description: Initiate the eunit tests, set upp needed processes etc
+%% Returns: non
+%% --------------------------------------------------------------------
