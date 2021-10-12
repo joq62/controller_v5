e6c9c946ae6c4dee3746db838ae5674903f44ead
joq62 <joakim.leche@gmail.com>
2021-10-12 23:37:06 +0200

    ud

--- test_src/test.erl
+++ test_src/test.erl
@@ -133,9 +133,62 @@ pass_1()->
      {mydivi,"1.0.0","https://github.com/joq62/mydivi.git",1,
       [controller@c0]}]=db_deployment:read_all(),
     WantedState=db_deployment:wanted_state(),
-    gl=[check_1(AppInfo)||AppInfo<-WantedState],
+    
+    FormatWantedState1=[format_wanted(AppInfo)||AppInfo<-WantedState],
+    FormatWantedState=lists:append(FormatWantedState1),
+    io:format("FormatWantedState ~p~n",[FormatWantedState]),
+    WantedWithHost=[{App,Host}||{App,Host}<-FormatWantedState],
+    WantedNoHosts=[App||{App}<-FormatWantedState],
+    io:format("WantedWithHost ~p~n",[WantedWithHost]),
+    io:format("WantedNoHosts ~p~n",[WantedNoHosts]),
+    SdAllApps=sd:all(), %[{Node,[{App,Info,Vsn}]}]
+    FormatActualState1=[format_sd(SdAppInfo)||SdAppInfo<-SdAllApps],
+    FormatActualState=lists:append(FormatActualState1),
+    io:format("FormatActualState ~p~n",[FormatActualState]),
+    {[{mydivi,[controller@c0]}],UpdatedActualState}=missing_w_hosts(WantedWithHost,FormatActualState,[]),
+    gl=missing_no_hosts(WantedNoHosts,UpdatedActualState,[]),
+    
     ok.
 
+
+with_hosts([],FormatActualState,MissingAcc)->
+    {MissingAcc,FormatActualState};
+with_hosts([{App,Host}|T],FormatActualState,MissingAcc)->
+    case lists:member({App,Host},FormatActualState) of
+	true->
+	    NewMissingAcc=MissingAcc,
+	    NewActualAcc=lists:delete({App,Host},FormatActualState);
+	false ->
+	    NewMissingAcc=[{App,Host}|MissingAcc],
+	    NewActualAcc=FormatActualState
+    end,
+    with_hosts(T,NewActualAcc,NewMissingAcc).
+
+format_sd({Node,AppInfoList})->
+    [{App,[Node]}||{App,_,_}<-AppInfoList].
+
+format_wanted({App,Replicas,[]})->
+    format_wanted(Replicas,App,[]);
+format_wanted({App,Replicas,Hosts})->
+    Diff=Replicas-lists:flatlength(Hosts),
+    Result=if
+	       Diff==0-> % onetoone
+		   [{App,[Host]}||Host<-Hosts];
+	       Diff>0-> % more replicas then Hosts
+		   L1=[{App,[Host]}||Host<-Hosts],
+		   L2=format_wanted({App,Diff,[]}),
+		   lists:append([L1,L2]);
+	       Diff<0 ->
+		   {error,[to_few_replicas,Diff]}
+	   end,
+    Result.
+
+format_wanted(0,_App,Result)->
+    Result;
+format_wanted(N,App,Acc)->
+    format_wanted(N-1,App,[{App}|Acc]).
+
+    
 check_1({App,Replicas,[]})->
     NumDeployed=lists:flatlength(sd:get(App)),
     {App,get_diff(Replicas,NumDeployed)};
