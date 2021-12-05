c61551a08d445e03f4865b6c57c00dc5fb0df7a8
joq62 <joakim.leche@gmail.com>
2021-12-05 01:39:16 +0100

    corr

--- include/controller.hrl
+++ include/controller.hrl
@@ -1,10 +1,10 @@
 -ifdef(unit_test).
--define(ControllerNodes,"test_controller.nodes").
+-define(ControllerNodes,"test_configurations/controller.nodes").
 -define(HostConfiguration,"test_configurations/host_configuration").
 -define(Deployments,"test_configurations/deployments").
 -define(ServiceCatalog,"test_configurations/service_catalog/service.catalog").
 -else.
--define(ControllerNodes,"controller.nodes").
+-define(ControllerNodes,"configurations/controller.nodes").
 -define(HostConfiguration,"configurations/host_configuration").
 -define(Deployments,"configurations/deployments").
 -define(ServiceCatalog,"configurations/service_catalog/service.catalog").
